a19efd11997ef1906ea1715cdf4c5814aefb64b5
Timo Peters <peters@teck-digital.de>
2022-08-10 19:45:45 +0200

    added missing files

---
+++ src/pages/Home.tsx
@@ -0,0 +1,18 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import ExploreContainer from '../components/ExploreContainer';
+import 
+
+const Home: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>GameScoring</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+ 	<ion-
+    </IonPage>
+  );
+};
+
+export default Home
